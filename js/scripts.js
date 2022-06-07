d4c260e928f931f38f2d99c36341a5a772a7e15a
Josephwjack <joseph.w.jack@gmail.com>
2022-06-07 16:33:13 -0700

    WIP stuck on omitting offensive words

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+function wordCounter(text) {
+  if (text.trim().length === 0) {
+    return 0;
+  }
+  let wordCount = 0;
+  const wordArray = text.split(" ");
+  wordArray.forEach(function(element) {
+    if (!Number(element)) {
+      wordCount++;
+    }
+  });
+  return wordCount;
+}
+
+function numberOfOccurencesInText(word, text) {
+  if (text.trim().length === 0) {
+    return 0;
+  }
+  const wordArray = text.split(" ");
+  let wordCount = 0;
+  wordArray.forEach(function(element) {
+    if (element.toLowerCase().includes(word.toLowerCase())) {
+      wordCount++;
+    }
+  });
+  return wordCount;
+}
+
+function omitBadWords(word, text) {
+  if (text.trim().length === 0) {
+    return 0;
+  
