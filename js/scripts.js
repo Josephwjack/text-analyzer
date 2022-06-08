3041004d3f147e86004624c6a01a1035daf5bd16
Josephwjack <joseph.w.jack@gmail.com>
2022-06-07 23:16:16 -0700

    add functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
+// Utility Logic
+
+function noInputtedWord(word, text) {
+  return ((text.trim().length === 0) || (word.trim().length === 0));
+}
+
+// Business Logic
+
 function wordCounter(text) {
   if (text.trim().length === 0) {
     return 0;
@@ -12,8 +20,8 @@ function wordCounter(text) {
   return wordCount;
 }
 
-function numberOfOccurencesInText(word, text) {
-  if (text.trim().length === 0) {
+function numberOfOccurrencesInText(word, text) {
+  if (noInputtedWord(word, text)) {
     return 0;
   }
   const wordArray = text.split(" ");
@@ -34,3 +42,36 @@ function omitBadWords(text) {
   });
   return wordArray;
 }
+// UI Logic
+
+function boldPassage(word, text) {
+  if (noInputtedWord(word, text)) {
+    return "";
+  }
+  let htmlString = "<p>";
+  let textArray = text.split(" ");
+  textArray.forEach(function(element, index) {
+    if (element.toLowerCase().includes(word.toLowerCase())) {
+      htmlString = htmlString.concat("<b>" + element + "</b>");
+    } else {
+      htmlString = htmlString.concat(element);
+    }
+    if (index !== (textArray.length - 1)) {
+      htmlString = htmlString.concat(" ");
+    }
+  });
+  return htmlString + "</p>";
+}
+
+$(document).ready(function(){
+  $("form#word-counter").submit(function(event){
+    event.preventDefault();
+    const passage = $("#text-passage").val();
+    const word = $("#word").val();
+    const wordCount = wordCounter(passage);
+    const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+    $("#total-count").html(wordCount);
+    $("#selected-count").html(occurrencesOfWord);
+    $("#bolded-passage").html(boldPassage(word, passage));
+  });
+});
